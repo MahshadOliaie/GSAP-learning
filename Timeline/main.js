eecafc7a0b3abf6f0fcd089454c51543a5acecae
MahshadOliaie <mahshadoliaie@gmail.com>
2025-08-28 19:14:55 +0330

    edit

--- Timeline/main.js
+++ Timeline/main.js
@@ -11,7 +11,7 @@ function openSideBar(){
      .fromTo('.sidebarLogo' , {y: '15' , opacity: 0} , {duration: 1 , opacity: 1 , y: 0})
      .to('.sidebarMain' , {duration: 0.5 , scale: 1 , ease: 'power1'} , 0)
      .to('.menu_item' , {duration: 0.2 , opacity: 1 , stagger: 0.2} , '0.5')
-     .fromTo('.sidebarMain-bottom' , {opacity: 0 , scale: 0.7} , {duration: 0.8 , opacity: 1 , scale: 1 , ease: 'power1'} , '<0.4')
+     .fromTo('.sidebarMain-bottom' , {opacity: 0 , scale: 0.7} , {duration: 0.8 , opacity: 1 , scale: 1 , ease: 'power1'} , '<0.2')
      .fromTo('.sidebarMain-bottom-image' , {scale: 1.7} , {duration: 0.8 , scale: 1 , ease: 'power1'} , '<0')
 }
 
